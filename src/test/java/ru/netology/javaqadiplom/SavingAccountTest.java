c045c24af3d2d3afd69a9cb12dced04b5f00d67b
wonderyana <ynappva@gmail.com>
2024-06-23 01:17:17 +0300

    Tests added to SavingAccountTest

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -1,7 +1,8 @@
 package ru.netology.javaqadiplom;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SavingAccountTest {
 
@@ -16,6 +17,116 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        assertEquals(2_000 + 3_000, account.getBalance());
+    }
+
+    @Test
+    public void testConstructorValidParameters() {
+        int initialBalance = 2_000;
+        int minBalance = 1_000;
+        int maxBalance = 10_000;
+        int rate = 5;
+
+        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
+
+        int expected = initialBalance;
+        int actual = account.getBalance();
+        assertEquals(expected, actual);
+
+        expected = minBalance;
+        actual = account.getMinBalance();
+        assertEquals(expected, actual);
+
+        expected = maxBalance;
+        actual = account.getMaxBalance();
+        assertEquals(expected, actual);
+
+        expected = rate;
+        actual = account.getRate();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConstructorInvalidRate() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(2_000, 1_000, 10_000, -1);
+        });
+    }
+
+    @Test
+    public void testConstructorInvalidMinBalance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(2_000, -500, 10_000, 5);
+        });
+    }
+
+    @Test
+    public void testConstructorInitialBalanceBelowMinBalance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(400, 1_000, 10_000, 5);
+        });
+    }
+
+    @Test
+    public void testConstructorInitialBalanceAboveMaxBalance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(16_000, 1_000, 10_000, 5);
+        });
+    }
+
+    @Test
+    public void testConstructorMinBalanceGreaterThanMaxBalance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(2_000, 16_000, 10_000, 5);
+        });
+    }
+
+    @Test
+    public void testPayValidAmount() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+        boolean result = account.pay(400);
+        int expected = 600;
+        int actual = account.getBalance();
+        assertEquals(expected, actual);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testPayInvalidAmount() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+        boolean result = account.pay(2_300);
+        int expected = 2_000;
+        int actual = account.getBalance();
+        assertEquals(expected, actual);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testAddValidAmount() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+        boolean result = account.add(400);
+        int expected = 2_400;
+        int actual = account.getBalance();
+        assertEquals(expected, actual);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testAddInvalidAmount() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+        boolean result = account.add(-600);
+        int expected = 2_000;
+        int actual = account.getBalance();
+        assertEquals(expected, actual);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void testYearChange() {
+        SavingAccount account = new SavingAccount(2_000, 1_000, 10_000, 5);
+        int expected = 100; // 2_000 * 5 / 100
+        int actual = account.yearChange();
+        assertEquals(expected, actual);
     }
 }
+
